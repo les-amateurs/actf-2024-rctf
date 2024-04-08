ed9861c53463379d16516495d1ea495a6b3e0dfc
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-04-07 18:18:36 -0700

    Update scores.ts

--- server/util/scores.ts
+++ server/util/scores.ts
@@ -1,13 +1,18 @@
-const p0 = 0.7
-const p1 = 0.96
+// larry scoring function
+const e = 200;
+const m = 50;
 
-const c0 = -Math.atanh(p0)
-const c1 = Math.atanh(p1)
-const a = (x: number): number => (1 - Math.tanh(x)) / 2
-const b = (x: number): number => (a((c1 - c0) * x + c0) - a(c1)) / (a(c0) - a(c1))
+const calculateV = (): number => {
+  return (e - m ** 2) / (2 * m - e - 1);
+};
+
+const calculateK = (V: number, rl: number, rh: number): number => {
+  return (((rh - rl) / 2) * Math.log(2)) / Math.log((m + V) / (1 + V));
+};
 
 export const getScore = (rl: number, rh: number, maxSolves: number, solves: number): number => {
-  const s = Math.max(1, maxSolves)
-  const f = (x: number): number => rl + (rh - rl) * b(x / s)
-  return Math.round(Math.max(f(solves), f(s)))
-}
+  const V = calculateV();
+  const K = calculateK(V, rl, rh);
+  const P = rh - K * Math.log((solves + V) / (1 + V));
+  return Math.round(P);
+};
